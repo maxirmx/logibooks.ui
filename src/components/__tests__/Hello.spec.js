b5d3518ec8bd1c93d0017e5ade0e83bfa144fa9b
Maxim [maxirmx] Samsonov <maxirmx@sw.consulting>
2025-06-21 23:32:49 +0300

    wip: cleanup

--- src/components/__tests__/Hello.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-//import { mount } from '@vue/test-utils'
-
-describe('Hello', () => {
-  it('shame that there is no unit tests', () => {
-    expect(true).toBe(true)
-  })
-})

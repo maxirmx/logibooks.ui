0daac338525059eb5ba52d6a0dfc44d00c9367a7
Maxim Samsonov <maxirmx@sw.consulting>
2025-06-21 19:16:14 +0300

    Add unit tests and coverage config

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -12,6 +12,11 @@ export default mergeConfig(
       root: fileURLToPath(new URL('./', import.meta.url)),
       transformMode: {
         web: [/\.[jt]sx$/]
+      },
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html'],
+        include: ['src/helpers/*.js', 'src/stores/alert.store.js']
       }
     }
   })

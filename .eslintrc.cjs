33e47f426410152f0b2227f08c6da35d007e1527
Maxim [maxirmx] Samsonov <maxirmx@sw.consulting>
2025-06-21 17:25:56 +0300

    chore: fix vite configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,16 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
+  // Enable ESM support for imports in a CJS file
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2022,
+  },
   root: true,
   ignorePatterns: ["doc/**"],
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+  ]
 }

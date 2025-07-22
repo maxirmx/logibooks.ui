04ee42dc1c59bd6c8ba9bcf7cee6c4d9412007e8
Maxim Samsonov <maxirmx@sw.consulting>
2025-07-22 17:00:47 +0300

    feat: render parcels list by company

---
+++ src/helpers/company.constants.js
@@ -0,0 +1,2 @@
+export const OZON_COMPANY_ID = 1
+export const WBR_COMPANY_ID = 2
--- src/views/Parcels_View.vue
+++ src/views/Parcels_View.vue
@@ -1,10 +1,33 @@
 <script setup>
-import ParcelsList from '@/components/WbrParcels_List.vue'
+import { ref, onMounted, computed } from 'vue'
+import OzonParcelsList from '@/components/OzonParcels_List.vue'
+import WbrParcelsList from '@/components/WbrParcels_List.vue'
+import { fetchWrapper } from '@/helpers/fetch.wrapper.js'
+import { apiUrl } from '@/helpers/config.js'
+import { OZON_COMPANY_ID, WBR_COMPANY_ID } from '@/helpers/company.constants.js'
+
 const props = defineProps({ id: { type: Number, required: true } })
+
+const companyId = ref(null)
+
+onMounted(async () => {
+  try {
+    const res = await fetchWrapper.get(`${apiUrl}/registers/${props.id}`)
+    companyId.value = res.companyId
+  } catch {
+    companyId.value = null
+  }
+})
+
+const listComponent = computed(() => {
+  if (companyId.value === OZON_COMPANY_ID) return OzonParcelsList
+  if (companyId.value === WBR_COMPANY_ID) return WbrParcelsList
+  return null
+})
 </script>
 
 <template>
   <Suspense>
-    <ParcelsList :register-id="props.id" />
+    <component v-if="listComponent" :is="listComponent" :register-id="props.id" />
   </Suspense>
 </template>

3303f954a9fc72d2c3b15b1cc63522a817224cf5
maxirmx <m.samsonov@computer.org>
2025-07-30 21:03:49 +0300

    fix lint issue

--- src/components/StopWord_Settings.vue
+++ src/components/StopWord_Settings.vue
@@ -24,7 +24,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-import { ref, computed, onMounted, watch, nextTick } from 'vue'
+import { ref, computed, onMounted, nextTick } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useForm, useField } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/yup'

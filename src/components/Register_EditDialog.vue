74a54a8858f2d1eabc5ddc69368b4f35b605f70b
Maxim [maxirmx] Samsonov <maxirmx@sw.consulting>
2025-08-03 21:24:10 +0300

    snapshot

--- src/components/Register_EditDialog.vue
+++ src/components/Register_EditDialog.vue
@@ -57,7 +57,7 @@ function getCustomerName(customerId) {
 </script>
 
 <template>
-  <div class="settings form-3 form-compact">
+  <div class="settings form-4 form-compact">
     <h1 class="primary-heading">Редактировать информацию о реестре</h1>
     <hr class="hr" />
     <Form

8b162727ee5c8d6fdd3839594fc1697473ea069c
Maxim Samsonov <maxirmx@sw.consulting>
2025-08-07 19:19:34 +0300

    feat: add parcel views store (#81)

---
+++ src/stores/parcel.views.store.js
@@ -0,0 +1,47 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { fetchWrapper } from '@/helpers/fetch.wrapper.js'
+import { apiUrl } from '@/helpers/config.js'
+
+const baseUrl = `${apiUrl}/parcelviews`
+
+export const useParcelViewsStore = defineStore('parcelViews', () => {
+  const orderView = ref(null)
+  const loading = ref(false)
+  const error = ref(null)
+
+  async function add(orderId) {
+    loading.value = true
+    error.value = null
+    try {
+      await fetchWrapper.post(baseUrl, { id: orderId })
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
+  }
+
+  async function back() {
+    loading.value = true
+    error.value = null
+    try {
+      const response = await fetchWrapper.put(baseUrl)
+      orderView.value = response || null
+      return orderView.value
+    } catch (err) {
+      error.value = err
+      return null
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    orderView,
+    loading,
+    error,
+    add,
+    back
+  }
+})

6a1e3da5056404cf8564dd3f38437ee367c4847a
Maxim [maxirmx] Samsonov <maxirmx@sw.consulting>
2025-08-07 19:23:34 +0300

    prev parcel

--- src/stores/parcel.views.store.js
+++ src/stores/parcel.views.store.js
@@ -1,3 +1,28 @@
+// Copyright (C) 2025 Maxim [maxirmx] Samsonov (www.sw.consulting)
+// All rights reserved.
+// This file is a part of Logibooks frontend application
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { fetchWrapper } from '@/helpers/fetch.wrapper.js'
@@ -6,15 +31,15 @@ import { apiUrl } from '@/helpers/config.js'
 const baseUrl = `${apiUrl}/parcelviews`
 
 export const useParcelViewsStore = defineStore('parcelViews', () => {
-  const orderView = ref(null)
+  const prevParcel = ref(null)
   const loading = ref(false)
   const error = ref(null)
 
-  async function add(orderId) {
+  async function add(parcelId) {
     loading.value = true
     error.value = null
     try {
-      await fetchWrapper.post(baseUrl, { id: orderId })
+      await fetchWrapper.post(baseUrl, { id: parcelId })
     } catch (err) {
       error.value = err
     } finally {
@@ -27,8 +52,8 @@ export const useParcelViewsStore = defineStore('parcelViews', () => {
     error.value = null
     try {
       const response = await fetchWrapper.put(baseUrl)
-      orderView.value = response || null
-      return orderView.value
+      prevParcel.value = response || null
+      return prevParcel.value
     } catch (err) {
       error.value = err
       return null
@@ -38,7 +63,7 @@ export const useParcelViewsStore = defineStore('parcelViews', () => {
   }
 
   return {
-    orderView,
+    prevParcel,
     loading,
     error,
     add,

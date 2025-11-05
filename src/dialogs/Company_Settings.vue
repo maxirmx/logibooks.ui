a5ab7495d414f64ec86c7913ebbce1fb37f6b943
Maxim Samsonov <m.samsonov@computer.org>
2025-11-05 20:31:00 +0300

    fix linter issue

--- src/dialogs/Company_Settings.vue
+++ src/dialogs/Company_Settings.vue
@@ -1,4 +1,5 @@
 <script setup>
+/* global FileReader */
 // Copyright (C) 2025 Maxim [maxirmx] Samsonov (www.sw.consulting)
 // All rights reserved.
 // This file is a part of Logibooks ui application 

68cf21c01cc7fb9b17ce35ed6fd842807da6de42
Maxim Samsonov <m.samsonov@computer.org>
2025-12-05 13:39:29 +0300

    chore: fix column headers in registers list

--- src/lists/Registers_List.vue
+++ src/lists/Registers_List.vue
@@ -421,8 +421,8 @@ const headers = [
   { title: 'Страны', key: 'countries' },
   { title: 'Отправитель/Получатель', key: 'senderRecipient' },
   { title: 'Товаров/Посылок', key: 'parcelsTotal' },
-  { title: 'Вес, кг, общий / К оформлению', key: 'weight', minWidth: '170px' },
-  { title: 'Стоимость общая / К оформлению', key: 'price', minWidth: '190px' },
+  { title: 'Вес, кг, общий / К оформлению', key: 'weight', width: '180px' },
+  { title: 'Стоимость общая / К оформлению', key: 'price', width: '200px' },
   { title: 'Дата загрузки', key: 'date' }
 ]
 

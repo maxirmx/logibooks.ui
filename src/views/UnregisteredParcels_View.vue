3df89c9000f9944e534508f1a2450dd16b0a3849
Maxim Samsonov <maxirmx@sw.consulting>
2026-04-03 02:58:09 +0300

    feat: add 'Закрыть' header action to warehouse and unregistered parcel lists (#450)

--- src/views/UnregisteredParcels_View.vue
+++ src/views/UnregisteredParcels_View.vue
@@ -4,12 +4,19 @@
 // This file is a part of Logibooks ui application
 
 import UnregisteredParcelsList from '@/lists/UnregisteredParcels_List.vue'
+import { useRouter } from 'vue-router'
 
 const props = defineProps({
   registerId: { type: Number, required: true }
 })
+
+const router = useRouter()
+
+function closeList() {
+  router.back()
+}
 </script>
 
 <template>
-  <UnregisteredParcelsList :register-id="props.registerId" />
+  <UnregisteredParcelsList :register-id="props.registerId" @close="closeList" />
 </template>

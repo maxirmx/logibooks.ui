737a3f7ec37648cd509433d582e8aa506bd945f9
maxirmx <m.samsonov@computer.org>
2026-07-24 15:54:53 +0300

    fix

--- tests/RegisterStatus_EditDialog.spec.js
+++ tests/RegisterStatus_EditDialog.spec.js
@@ -241,6 +241,7 @@ describe('RegisterStatus_EditDialog.vue', () => {
       expect(checkbox.classes()).toContain('checkbox-styled')
       expect(wrapper.get('.status-settings-label[for="readOnly"]').text()).toBe('Изменения запрещены:')
       expect(wrapper.get('.checkbox-styled + label').attributes('aria-hidden')).toBe('true')
+    })
 
     it('renders current icon and color values in edit mode', async () => {
       const wrapper = mount(AsyncWrapper, {
